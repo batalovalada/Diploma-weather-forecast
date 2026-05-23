491ddb83549fcc565460ef8d78de7f6eed45f302
batalovalada <batalova.lada@yandex.ru>
2026-05-23 18:09:28 +0300

    add seasonal balanced datasets for models

---
+++ models/ConvLSTM/create_seasonal_balanced_dataset.py
@@ -0,0 +1,142 @@
+import xarray as xr
+import numpy as np
+import copy
+
+from config.data.features_config import features, spatial_features, temporal_features
+from config.data.split_seasonal_year_config import *
+
+path_selected = '../../data/preprocessed/year/selected/'
+
+# year base
+# path_norm = '../../data/ConvLSTM/seasonal_year/base/norm_params/'
+# path_processed = '../../data/ConvLSTM/seasonal_year/base/processed/'
+
+# year spatial
+# path_norm = '../../data/ConvLSTM/seasonal_year/spatial/norm_params/'
+# path_processed = '../../data/ConvLSTM/seasonal_year/spatial/processed/'
+
+# year temporal
+path_norm = '../../data/ConvLSTM/seasonal_year/temporal/norm_params/'
+path_processed = '../../data/ConvLSTM/seasonal_year/temporal/processed/'
+
+# year spatiotemporal
+# path_norm = '../../data/ConvLSTM/seasonal_year/spatiotemporal/norm_params/'
+# path_processed = '../../data/ConvLSTM/seasonal_year/spatiotemporal/processed/'
+
+
+# ======= parameters ================================================
+lookback = 4
+horizon = 1
+
+# normalize function
+def normalize(data, mean, std):
+    return (data - mean) / std
+
+# ============== create norm_params function =============
+def create_blocks_sequences(X, y, mask, time_blocks, lookback, horizon):
+    X_seq, y_seq, mask_seq, time_seq = [], [], [], []
+
+    for start_block, end_block in time_blocks:
+        # sequences only from continuous time interval
+        X_block = X.sel(time=slice(start_block, end_block))
+        y_block = y.sel(time=slice(start_block, end_block))
+        mask_block = mask.sel(time=slice(start_block, end_block))
+
+        for i in range(len(X_block.time) - lookback - horizon + 1):
+            X_seq.append(X_block.isel(time=slice(i, i+lookback)).values)
+
+            y_t = i+lookback+horizon-1
+            y_seq.append(y_block.isel(time=y_t).values)
+            mask_seq.append(mask_block.isel(time=y_t).values)
+            time_seq.append(X_block.time.values[y_t])
+    return (
+        np.array(X_seq, dtype=np.float32),
+        np.array(y_seq, dtype=np.float32),
+        np.array(mask_seq, dtype=bool),
+        np.array(time_seq, dtype='datetime64[ns]'),
+    )
+
+# =============================== load data ============================
+ds_wrf = xr.open_dataset(path_selected+'ds_selected_wrf.nc')
+ds_era5 = xr.open_dataset(path_selected+'ds_selected_era5.nc')
+
+# to choose, what dataset do you need:
+# base
+# ds_wrf = ds_wrf.drop_vars(spatial_features+temporal_features)
+# spatial
+# ds_wrf = ds_wrf.drop_vars(temporal_features)
+# # temporal
+ds_wrf = ds_wrf.drop_vars(spatial_features)
+# spatiotemporal - nothing
+
+# creating X, y with train, val, test ======================================
+X_ds = copy.deepcopy(ds_wrf)
+y_ds = ds_era5['t2m'] - ds_wrf['T2']
+
+y_mask = (~np.isnan(y_ds)) # -> loss
+y_ds = y_ds.fillna(0) # y_ds has nan
+
+# ========================== normalize =======================================
+# select train data to define mean, std
+train_X_list = []
+train_y_list = []
+
+for start_block, end_block in train_blocks:
+    train_X_list.append(X_ds.sel(time=slice(start_block, end_block)))
+    train_y_list.append(y_ds.sel(time=slice(start_block, end_block)))
+
+X_train_blocks = xr.concat(train_X_list, dim='time')
+y_train_blocks = xr.concat(train_y_list, dim='time')
+
+# define std, mean for train data X and y
+X_train_blocks = X_train_blocks.to_array().transpose("time","variable","south_north","west_east")
+X_mean = X_train_blocks.sel(variable=features).mean(dim=['time', 'south_north', 'west_east'])
+X_std = X_train_blocks.sel(variable=features).std(dim=['time', 'south_north', 'west_east'])
+
+y_mean = y_train_blocks.mean()
+y_std = y_train_blocks.std()
+
+# normalize main features data
+X_all = X_ds.to_array().transpose("time","variable","south_north","west_east")
+
+X_all.loc[dict(variable=features)] = normalize(X_all.sel(variable=features), X_mean, X_std)
+
+# normalize separated spatial features (comment if dataset is base/temporal)
+# X_spatial_mean = X_train_blocks.sel(variable=spatial_features).mean(dim=['time', 'south_north', 'west_east'])
+# X_spatial_std = X_train_blocks.sel(variable=spatial_features).std(dim=['time', 'south_north', 'west_east'])
+#
+# X_all.loc[dict(variable=spatial_features)] = normalize(X_all.sel(variable=spatial_features), X_spatial_mean, X_spatial_std)
+
+# normalize target
+y_all = normalize(y_ds, y_mean, y_std)
+
+# ================================= create sequences  =================================
+
+X_train_seq, y_train_seq, mask_train_seq, time_train_seq = create_blocks_sequences(X_all, y_all, y_mask, train_blocks, lookback, horizon)
+X_val_seq, y_val_seq, mask_val_seq, time_val_seq  = create_blocks_sequences(X_all, y_all, y_mask, val_blocks, lookback, horizon)
+X_test_seq, y_test_seq, mask_test_seq, time_test_seq  = create_blocks_sequences(X_all, y_all, y_mask, test_blocks, lookback, horizon)
+
+# T2 wrf, era5 to ndarray for check model result ============================
+t2_wrf= ds_wrf.sel(time=time_test_seq)['T2'].values
+t2_era5 = ds_era5.sel(time=time_test_seq)['t2m'].values
+
+# ========================= save normalize parameters ====================
+norm_params_X = {
+    'mean': X_mean,
+    'std': X_std
+}
+
+norm_params_y = {
+    'mean': y_mean,
+    'std': y_std
+}
+np.save(path_norm+'norm_params_X.npy', norm_params_X)
+np.save(path_norm+'norm_params_y.npy', norm_params_y)
+
+# ========================= save preprocess data ====================
+np.savez(path_processed+'train.npz', X=X_train_seq, y=y_train_seq, mask=mask_train_seq, time=time_train_seq)
+np.savez(path_processed+'val.npz', X=X_val_seq, y=y_val_seq, mask=mask_val_seq, time=time_val_seq)
+np.savez(path_processed+'test.npz', X=X_test_seq, y=y_test_seq, mask=mask_test_seq, time=time_test_seq)
+
+np.save(path_processed+'t2_wrf_test.npy', t2_wrf)
+np.save(path_processed+'t2_era5_test.npy', t2_era5)
---
+++ models/tree_models/create_seasonal_balanced_dataset.py
@@ -0,0 +1,152 @@
+import numpy as np
+import xarray as xr
+
+from config.data.features_config import features
+from config.hyperparameters.tree_models import lags
+from config.data.split_seasonal_year_config import *
+
+path_selected = '../../data/preprocessed/year/selected/'
+
+# ============= paths =======================
+
+# year base !!!
+# path_processed = '../../data/tree_models/seasonal_year/base/processed/'
+
+# year spatial !!!
+# path_processed = '../../data/tree_models/seasonal_year/spatial/processed/'
+
+# year temporal !!!
+path_processed = '../../data/tree_models/seasonal_year/temporal/processed/'
+
+# year spatiotemporal !!!
+# path_processed = '../../data/tree_models/seasonal_year/spatiotemporal/processed/'
+
+#===========functions =========================
+def create_block_lags(ds, target, time_blocks):
+    X_list = []
+    y_list = []
+    time_list = []
+
+    for start_block, end_block in time_blocks:
+        # lags only from continuous time interval
+        ds_block = ds.sel(time=slice(start_block, end_block))
+        target_block = target.sel(time=slice(start_block, end_block))
+
+        for t in range(lags, len(ds_block.time)):
+            X_t = []
+            for lag in range(lags):
+                X_t.append(ds_block[features].isel(time=t - lag).to_array().values)
+
+            X_t = np.concatenate(X_t, axis=0)  # shape = (features*lags, H, W)
+            y_t = target_block.isel(time=t).values  # shape = (H, W)
+
+            X_list.append(X_t)
+            y_list.append(y_t)
+            time_list.append(ds_block.time.isel(time=t).values)
+
+    # add time axis
+    X = np.stack(X_list)  # (time, features*lags, H, W)
+    y = np.stack(y_list)  # (time, H, W)
+    return X, y, np.array(time_list)
+
+def flatten_data(X, y):
+    t, f, h, w = X.shape
+    X_flat = X.transpose(0, 2, 3, 1).reshape(-1, f)  # (samples, features), samples = time x H x W
+    y_flat = y.reshape(-1)  # (time x H x W, )
+    return X_flat, y_flat, t, h, w
+
+def add_coords(X, lat_flatten, lon_flatten, time):
+    # add coordinate to each time moment
+    lat_tiled = np.tile(lat_flatten, time)
+    lon_tiled = np.tile(lon_flatten, time)
+
+    return np.concatenate([X, lat_tiled[:, None], lon_tiled[:, None]], axis=1)
+
+def add_time_features(X, hour_sin, hour_cos, day_sin, day_cos):
+    hour_sin_flat = hour_sin.reshape(-1, 1)
+    hour_cos_flat = hour_cos.reshape(-1, 1)
+    day_sin_flat = day_sin.reshape(-1, 1)
+    day_cos_flat = day_cos.reshape(-1, 1)
+    return np.concatenate([
+        X,
+        hour_sin_flat,
+        hour_cos_flat,
+        day_sin_flat,
+        day_cos_flat,
+    ], axis=1)
+
+def apply_nan_mask(X, y):
+    mask = ~np.isnan(y)
+    return X[mask], y[mask], mask
+
+# ============ load data ================
+ds_wrf = xr.open_dataset(path_selected+'ds_selected_wrf.nc')
+ds_era5 = xr.open_dataset(path_selected+'ds_selected_era5.nc')
+
+# =============== create target =============
+target = ds_era5['t2m'] - ds_wrf['T2']
+
+#======================= split and create time lags ===============================
+X_train, y_train, time_rf_train = create_block_lags(ds_wrf, target, train_blocks)
+X_val, y_val, time_rf_val = create_block_lags(ds_wrf, target, val_blocks)
+X_test, y_test, time_rf_test = create_block_lags(ds_wrf, target, test_blocks)
+
+# ============= flatten ====================
+X_train, y_train, T_train, H, W = flatten_data(X_train, y_train)
+X_val, y_val, T_val, _, _ = flatten_data(X_val, y_val)
+X_test, y_test, T_test, _, _ = flatten_data(X_test, y_test)
+
+# ========= add extra features ================
+# add coordinates features (spatial)
+# lats = ds_wrf.lat.isel(time=0).values # shape = (H, W)
+# lons = ds_wrf.lon.isel(time=0).values
+#
+# lat_flatten = lats.flatten() # shape = (H*W, )
+# lon_flatten = lons.flatten()
+#
+# X_train = add_coords(X_train, lat_flatten, lon_flatten, T_train)
+# X_val = add_coords(X_val, lat_flatten, lon_flatten, T_val)
+# X_test = add_coords(X_test, lat_flatten, lon_flatten, T_test)
+
+# # add time and day features (temporal)
+hour_sin_train = ds_wrf.hour_sin.sel(time=time_rf_train).values
+hour_cos_train = ds_wrf.hour_cos.sel(time=time_rf_train).values
+
+hour_sin_val = ds_wrf.hour_sin.sel(time=time_rf_val).values
+hour_cos_val = ds_wrf.hour_cos.sel(time=time_rf_val).values
+
+hour_sin_test = ds_wrf.hour_sin.sel(time=time_rf_test).values
+hour_cos_test = ds_wrf.hour_cos.sel(time=time_rf_test).values
+
+day_sin_train = ds_wrf.day_sin.sel(time=time_rf_train).values
+day_cos_train = ds_wrf.day_cos.sel(time=time_rf_train).values
+
+day_sin_val = ds_wrf.day_sin.sel(time=time_rf_val).values
+day_cos_val = ds_wrf.day_cos.sel(time=time_rf_val).values
+
+day_sin_test = ds_wrf.day_sin.sel(time=time_rf_test).values
+day_cos_test = ds_wrf.day_cos.sel(time=time_rf_test).values
+
+X_train = add_time_features(X_train, hour_sin_train, hour_cos_train, day_sin_train, day_cos_train)
+X_val = add_time_features(X_val, hour_sin_val, hour_cos_val, day_sin_val, day_cos_val)
+X_test = add_time_features(X_test, hour_sin_test, hour_cos_test, day_sin_test, day_cos_test)
+
+# ============ delete samples with nan values ========
+# era5 has nan values => y has too
+X_train, y_train, mask_train = apply_nan_mask(X_train, y_train)
+X_val, y_val, mask_val = apply_nan_mask(X_val, y_val)
+X_test, y_test, mask_test = apply_nan_mask(X_test, y_test)
+
+# ========= T2 to ndarray, add mask (ERA5 has nan)=======================
+t2_wrf_test = ds_wrf.sel(time=time_rf_test)['T2'].values # -> shape (time, H, W)
+t2_era5_test = ds_era5.sel(time=time_rf_test)['t2m'].values # -> shape (time, H, W)
+t2_mask_test = ~np.isnan(t2_era5_test) # shape (time, H, W)
+
+# =========== save data ====================
+np.save(path_processed+'t2_wrf_test.npy', t2_wrf_test)
+np.save(path_processed+'t2_era5_test.npy', t2_era5_test)
+np.save(path_processed+'t2_mask_test.npy', t2_mask_test)
+
+np.savez(path_processed+'train.npz', X=X_train, y=y_train, mask=mask_train, time=time_rf_train)
+np.savez(path_processed+'val.npz', X=X_val, y=y_val, mask=mask_val, time=time_rf_val)
+np.savez(path_processed+'test.npz', X=X_test, y=y_test, mask=mask_test, time=time_rf_test)
